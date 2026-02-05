559716b6709f0e92c9b0506e01cbc341769fb184
Raja <rajababuchauhan500@gmail.com>
2026-02-05 13:14:39 +0000

    running on cloud

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,10 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [tailwindcss()],
+    base: './', // <--- Use relative path
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        allowedHosts: ['p-5173.rajababu.space']
+    }
 });
